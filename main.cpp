63958d5c324a348e6392353c7556a45161acb411
Oleg595 <60892746+Oleg595@users.noreply.github.com>
2020-12-17 03:26:38 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,20 +1,24 @@
 #include <iostream>
-#include"png_toolkit.h"
+#include "png_toolkit.h"
+#include"Config.h"
 
-int main(int argc, char* argv[])
+int main( int argc, char *argv[] )
 {
     // toolkit filter_name base_pic_name sudent_tool student_pic_name limitPix limitMSE
     // toolkit near test images!
     try
     {
-        if (argc != 3)
-            throw "Not enough arguments";
+        if (argc != 4)
+            throw "Not enough arguments"; 
 
+        Config* conf = new Config(argv[1]);
         png_toolkit studTool;
-        studTool.load(argv[1]);
-        studTool.save(argv[2]);
+        studTool.load(argv[2]);
+        studTool.Paint(conf);
+        studTool.save(argv[3]);
+        delete conf;
     }
-    catch (const char* str)
+    catch (const char *str)
     {
         std::cout << "Error: " << str << std::endl;
         return 1;
